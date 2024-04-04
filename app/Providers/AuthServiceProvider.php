d2a1b124aee2229f7439f6d4516c09acac064dc4
Jonh rdz <j.rodriguez2018ng@clases.edu.sv>
2024-04-03 20:57:22 -0600

    first commit

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -28,7 +28,7 @@ class AuthServiceProvider extends ServiceProvider
         //agregamos el usuario Super Admin
         // Otorga implícitamente todos los permisos a la función "Superadministrador"       
         Gate::before(function ($user, $ability) {
-            return $user->email == 'adming@gmail.com' ?? null;
+            return $user->email == 'admin@gmail.com' ?? null;
         });
     }
 }
